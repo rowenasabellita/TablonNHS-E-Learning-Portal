aacd33922463bb6b581ea9eeb226dfc116db87df
Rowena Sabellita <sabellita.ro@gmaiil.com>
2021-10-27 13:06:51 +0800

    modify urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-
 from . import  views
 
 urlpatterns = [
-    path('', views.home, name = 'home')
+    path('', views.home, name = 'home'),
+    path('login', views.login, name = 'login'),
+    path('teacher', views.teacher, name = 'teacher'),
+    path('teacher', views.teachersubject, name = 'teachersubject'),
 ]
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include("myapp.urls")),
     path('admin/', admin.site.urls),
+
 ]
+    
