8dd8bfe462552b3dc210dd1cec2ca9fc8e0da332
mjvarquez <markjeraldv22@gmail.com>
2021-12-02 23:28:57 +0800

    model

--- myapp/models/model1.py
+++ myapp/models/model1.py
@@ -68,7 +68,7 @@ myapp_user = User()
 class User(AbstractUser):
     is_student = models.BooleanField(default=False)
     is_teacher = models.BooleanField(default=False)
-
+    
 
 class StudentProfile(models.Model):
     user = models.OneToOneField(
