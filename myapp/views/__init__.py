4e28efe95b3e4ac99f9ad83bca77dabaead2e2f4
mjvarquez <markjeraldv22@gmail.com>
2021-12-12 22:14:16 +0800

    add classrecord view

--- myapp/views/__init__.py
+++ myapp/views/__init__.py
@@ -3,4 +3,5 @@ from .student_mgmt_view import *
 from .upload_module_view import *
 from .record_submission_view import *
 from .student_module_view import *
+from .class_record_view import*
 from .errors import *
---
+++ myapp/views/class_record_view.py
@@ -0,0 +1,29 @@
+from django.dispatch.dispatcher import receiver
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from django.contrib import messages
+from django.utils.html import html_safe
+from django.utils.translation import ugettext
+from django.shortcuts import render
+import myapp
+from myapp.models.class_record_model import ClassRecord
+from myapp.models.subject_model import SUBJECTS, Subject
+from myapp.models import UserProfile
+from myapp.views.profile_view import login
+from myproject.settings import AUTH_PASSWORD_VALIDATORS
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
+from myapp.forms import ProfileUpdateForm, UserUpdateForm, UploadFileForm, RecordForm
+from django.contrib.auth import get_user_model
+from myapp.models.class_record_model import ClassRecord
+from myapp.filters import RecordFilter
+from django.core.files.storage import FileSystemStorage
+from myapp.views.upload_module_view import get_student_records, get_subjects, get_grade_quarterly_per_subject
+import json
+
+User = get_user_model()
+
+@login_required
+def view_classrecord(request):
+
+    return render(request, 'classrecord.html')
